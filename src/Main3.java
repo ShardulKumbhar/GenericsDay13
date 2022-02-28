e12eca0a597eef292bcad845156ee701ff01dbcc
Shardulkumbhar <kumbharshardul384@gmail.com>
2022-02-28 17:49:15 +0530

    added 2nd position to be maximum

--- src/Main3.java
+++ src/Main3.java
@@ -3,7 +3,7 @@ public class Main3 {
 	public static void main(String[] args) {
 		System.out.println("Check maximum of three Strings");
 
-		String a ="Peach", b = "Apple", c ="Banana"; // String value is passed
+		String a ="Apple", b = "Peach", c ="Banana"; // String value is passed
 
 		TestCase3.testMaximum(a, b, c); // calling constructor
 
