01774edd6be2ba431bb75f85e1173698931c5b9f
Shardulkumbhar <kumbharshardul384@gmail.com>
2022-02-28 15:22:06 +0530

    maximum in 3 strings problem added

---
+++ src/Main3.java
@@ -0,0 +1,12 @@
+
+public class Main3 {
+	public static void main(String[] args) {
+		System.out.println("Check maximum of three Strings");
+
+		String a ="Apple", b = "Peach", c ="Banana"; // String value is passed
+
+		TestCase3.testMaximum(a, b, c); // calling constructor
+
+	}
+
+}
