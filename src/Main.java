26ff87f4e432fd83101d8c34d933119fc50c1be5
Shardulkumbhar <kumbharshardul384@gmail.com>
2022-02-28 14:53:23 +0530

    addded to fin maximum numbher problem

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Check maximum of three variables");
+
+		Integer a = 25, b = 40, c = 50;
+
+		TestCase.testMaximum(a, b, c);   //calling constructor
+
+	}
+
+}
