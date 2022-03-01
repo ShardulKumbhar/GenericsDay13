622910ee4e7b31e52a42b187ac0d717f56d21206
Shardulkumbhar <kumbharshardul384@gmail.com>
2022-03-01 09:20:08 +0530

    added refactor2 file

--- src/Main4.java
+++ src/Main4.java
@@ -3,14 +3,15 @@ public class Main4 {
 	public static void main(String[] args) {
 		System.out.println("Check maximum of three");
 
-		// various parameters
-		Integer a = 26, b = 24, c = 18;
-		Float p = 1.24f, q = 2.25f, r = 0.1f;
-		String x = "Apple", y = "Peach", z = "Banana";
+		Integer a = 20, b = 40, c = 45;
+		Float p = 35.15f, q = 45.50f, r = 35.25f;
+		String x = "Peach", y = "Apple", z = "Banana";
 
-		TestCase4.<String>testMaximum(x, y, z);
-		TestCase4.<Integer>testMaximum(a, b, c);
-		TestCase4.<Float>testMaximum(p, q, r);
+		TestCase4<Integer> obj1 = new TestCase4<Integer>();
+		obj1.TestCase4(a, b, c);
+		TestCase4<Float> obj2 = new TestCase4<Float>();
+		obj2.TestCase4(p, q, r);
+		TestCase4<String> obj3 = new TestCase4<String>();
+		obj3.TestCase4(x, y, z);
 	}
-
 }
--- src/TestCase4.java
+++ src/TestCase4.java
@@ -1,27 +1,36 @@
 
 public class TestCase4<T extends Comparable<T>> {
-	T x, y, z;
+	public T a;
+	public T b;
+	public T c;
+	public T max;
 
-	// constructor test maximum
-	public TestCase4(T x, T y, T z) {
-		this.x = x;
-		this.y = y;
-		this.z = z;
+	public TestCase4() {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+		this.max = max;
 	}
 
-	public static <T extends Comparable<T>> T testMaximum(T x, T y, T z) {
-		T max = x;// Initializing x as greater
-		if (y.compareTo(max) > 0)
-			max = y;// max is greater
-
-		if (z.compareTo(max) > 0)
-			max = z;// max is greater
-		printMax(x, y, z, max);
+	public <T extends Comparable<T>> T TestCase4(T a, T b, T c) // constructor
+	{
+		T max = a; // inital value of taken to be max
+		if (b.compareTo(max) > 0) // comparing b value with max
+		{
+			max = b;
+		}
+		if (c.compareTo(max) > 0) // comparing c value with max
+		{
+			max = c; // max value store in max
+		}
+		toPrint(a, b, c, max);
 		return max;
 
 	}
 
-	public static <T> void printMax(T x, T y, T z, T max) {
-		System.out.printf("\nGiven input %s %s %s \nMaximum Number is : %s\n", x, y, z, max);
+	public <T> void toPrint(T a, T b, T c, T max) // to print output in format
+	{
+		System.out.printf("\nGiven values are: %s %s %s", a, b, c);
+		System.out.println("\nMaximum Value: " + max);
 	}
 }
